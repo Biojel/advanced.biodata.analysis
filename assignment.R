0ecb74a7fe0b5fd93039539ba44ee1f48c8fd0f9
MichaelRaimondi <46264586+MichaelRaimondi@users.noreply.github.com>
2018-12-30 16:58:24 +0100

    Add files via upload

---
+++ assignment.R
@@ -0,0 +1,14 @@
+# 1) test if local environmental conditions differ significantly between dune and forest habitat
+
+soil <- read.csv(file = "soil.csv")
+soil.manova <- manova(cbind(Moisture, OM, pH, P, NO3, NH4)~as.factor(Habitat), data=soil);
+summary(soil.manova, test="Wilks");
+
+summary(soil.manova, test="Pillai");
+summary(soil.manova, test="Hotelling-Lawley");
+summary(soil.manova, test="Roy");
+
+summary(aov(cbind(Moisture, OM, pH, P, NO3, NH4)~as.factor(Habitat), data=soil));
+
+soil_fungi <- read.csv(file = "soil_fungi.csv")
+
